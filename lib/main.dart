c8540c9b44869eedd7be0007cea4e234c6257ecc
Code With Azan <azanjokhio54@gmail.com>
2025-09-30 06:25:04 +0500

    Remove # sign from URL

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:e_commerce_web/app.dart';
 import 'package:e_commerce_web/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 Future<void> main() async {
   /// Ensure the widget is initialize
   WidgetsFlutterBinding.ensureInitialized();
+  // Remove # sign from URL
+  setPathUrlStrategy();
 
   /// Initialize Firebase and Authentication Repository
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
